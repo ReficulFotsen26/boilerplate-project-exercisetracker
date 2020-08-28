ed42efcff6c148248c8e995039f9ec1e3729a8fb
Reficulfotsen26 <jigrijalva932@students.florencek12.org>
2020-08-28 09:19:04 -0500

    Created the schemas and models. Started to make the express route.

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MLAB_URI, { useNewUrlParser: true, useUnifiedTopology: true }); 
+let uri = "mongodb+srv://REFOT26:' + process.env.PASS + 'lusteret.sdl1n.mongodb.net/REFOT26?retryWrites=true&w=majority"
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }); 
 
 app.use(cors())
 
@@ -14,7 +15,29 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+// MongoDB  
 
+// Schemas
+let exerciseLogSchema = new mongoose.Schema({
+  description: {type: String, required: true},
+  duration: {type: Number, required},
+  date: String
+});
+ 
+let userSchema = new mongoose.Schema({
+  username: { type: String, required: true},
+  log: [exerciseLogSchema]
+});
+
+// Models
+let User = mongoose.model('User', userSchema);
+let Logs = mongoose.model('Logs', exerciseLogSchema);
+
+// MongoDB ^^
+
+app.post("/api/exercise/new-user", bodyParser.urlencoded({extended: false}), (req, res) => {
+
+} )
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
